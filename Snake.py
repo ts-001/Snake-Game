4fb53f507c329894aab9dc9ae93a476fc6bb65fd
ts-001 <147905477+ts-001@users.noreply.github.com>
2023-10-17 23:20:47 +0530

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,2 +1,62 @@
-sn
+from turtle import Turtle
+
+POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+class Snake:
+    def __init__(self):
+        self.segment = []
+        self.create_snake()
+        self.head = self.segment[0]
+
+    def create_snake(self):
+        for pos in POSITION:
+            self.add_segment(pos)
+
+    def add_segment(self, pos):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(pos)
+        self.segment.append(new_segment)
+
+    def reset(self):
+        for seg in self.segment:
+            seg.goto(1000,1000)
+        self.segment.clear()
+        self.create_snake()
+        self.head = self.segment[0]
+
+    def extend(self):
+        self.add_segment((self.segment[-1]).pos())
+
+    def move(self):
+        for seg_num in range(len(self.segment) - 1, 0, -1):
+            new_x = self.segment[seg_num - 1].xcor()
+            new_y = self.segment[seg_num - 1].ycor()
+            self.segment[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+
+
 
